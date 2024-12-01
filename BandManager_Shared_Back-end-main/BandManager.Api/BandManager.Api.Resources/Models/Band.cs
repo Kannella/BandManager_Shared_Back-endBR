248115497459b772bd341a04ff79c3e1c8828556
Giovanni Canella <gi.canella84@gmail.com>
2024-12-01 18:14:40 -0300

    Removendo atributos do modelo Band

--- BandManager_Shared_Back-end-main/BandManager.Api/BandManager.Api.Resources/Models/Band.cs
+++ BandManager_Shared_Back-end-main/BandManager.Api/BandManager.Api.Resources/Models/Band.cs
@@ -3,6 +3,4 @@ namespace BandManager.Api.Resources.Models;
 public class Band : Entity
 {
     public string Name { get; set; } = null!;
-    public string? BandUsers { get; set; }
-    public string? Bookings { get; set; }
 }
